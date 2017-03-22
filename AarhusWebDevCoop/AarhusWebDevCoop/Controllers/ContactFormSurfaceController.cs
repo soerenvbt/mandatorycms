f8cab1d3cf6f9dd44c156d0941987e73fc73cb55
søren tygesen <sren tygesen>
2017-03-22 16:16:32 +0100

    first commit

---
+++ AarhusWebDevCoop/AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+
+
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+
+            MailMessage message = new MailMessage();
+            message.To.Add("sorenv_b_t@hotmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+            comment.SetValue("messageName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("messageContent", model.Message);
+
+            Services.ContentService.Save(comment);
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("therealbawsdawg@gmail.com", "umbracolol123");
+                smtp.EnableSsl = true;
+                // send mail
+                smtp.Send(message);
+            }
+            TempData["success"] = true;
+
+
+
+            return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
